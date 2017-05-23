82c51ac82ec57ac932b8e7380ce6bc69d40c59df
Tim <timtrittipo@users.noreply.github.com>
2017-05-23 15:00:16 -0700

    do not use windows find

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -3,6 +3,11 @@
 PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/cygdrive/c/Windows/system32:/cygdrive/c/Windows:/cygdrive/c/Windows/System32/Wbem:/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0:/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0:/cygdrive/c/bin/Git/cmd:/cygdrive/c/bin/AWSCLI
 export PATH
 
+if [[ "${OS}" == "Windows_NT" ]]; then
+  findBin=/usr/bin/find
+else
+  findBin=find
+fi
 
 ##################################################################################
 function usage(){
@@ -32,8 +37,8 @@ done
 cp "$JENKINS_HOME/"*.xml "$ARC_DIR"
 
 cp "$JENKINS_HOME/plugins/"*.[hj]pi "$ARC_DIR/plugins"
-hpi_pinned_count=$(find $JENKINS_HOME/plugins/ -name "*.hpi.pinned" | wc -l)
-jpi_pinned_count=$(find $JENKINS_HOME/plugins/ -name "*.jpi.pinned" | wc -l)
+hpi_pinned_count=$(${findBin} $JENKINS_HOME/plugins/ -name "*.hpi.pinned" | wc -l)
+jpi_pinned_count=$(${findBin} $JENKINS_HOME/plugins/ -name "*.jpi.pinned" | wc -l)
 if [ $hpi_pinned_count -ne 0 ] ||  [ $jpi_pinned_count -ne 0 ]; then
   cp "$JENKINS_HOME/plugins/"*.[hj]pi.pinned "$ARC_DIR/plugins"
 fi
@@ -54,11 +59,11 @@ function backup_jobs {
   local run_in_path=$1
   local rel_depth=${run_in_path#$JENKINS_HOME/jobs/}
   cd "$run_in_path"
-  find . -maxdepth 1 -type d | while read job_name ; do
+  ${findBin} . -maxdepth 1 -type d | while read job_name ; do
     [ "$job_name" = "." ] && continue
     [ "$job_name" = ".." ] && continue
     [ -d "$JENKINS_HOME/jobs/$rel_depth/$job_name" ] && mkdir -p "$ARC_DIR/jobs/$rel_depth/$job_name/"
-    find "$JENKINS_HOME/jobs/$rel_depth/$job_name/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} cp {} "$ARC_DIR/jobs/$rel_depth/$job_name/"
+    ${findBin} "$JENKINS_HOME/jobs/$rel_depth/$job_name/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} cp {} "$ARC_DIR/jobs/$rel_depth/$job_name/"
     if [ -f "$JENKINS_HOME/jobs/$rel_depth/$job_name/config.xml" ] && [ "$(grep -c "com.cloudbees.hudson.plugins.folder.Folder" "$JENKINS_HOME/jobs/$rel_depth/$job_name/config.xml")" -ge 1 ] ; then
       #echo "Folder! $JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
       backup_jobs "$JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
